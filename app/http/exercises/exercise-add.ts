5aa678adbc74fbe987e8626f5a2253fd4fb8b690
Bartosz Gordon <gordonbartosz@gmail.com>
2026-07-10 19:06:52 +0200

    Setup exercise add command

--- app/http/exercises/exercise-add.ts
+++ app/http/exercises/exercise-add.ts
@@ -1,4 +1,4 @@
-import type * as bg from "@bgord/bun";
+import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import type hono from "hono";
 import * as v from "valibot";
@@ -9,6 +9,7 @@ type Dependencies = {
   IdProvider: bg.IdProviderPort;
   Clock: bg.ClockPort;
   TemporaryFile: bg.TemporaryFilePort;
+  CommandBus: bg.CommandBusPort<Exercises.Commands.ExerciseAddCommandType>;
 };
 
 export const ExerciseAdd = (deps: Dependencies) => async (c: hono.Context<infra.Config>) => {
@@ -24,6 +25,14 @@ export const ExerciseAdd = (deps: Dependencies) => async (c: hono.Context<infra.
   const description = v.parse(Exercises.VO.ExerciseDescription, body.get("description"));
 
   const temporary = await deps.TemporaryFile.write(filename, file);
+
+  const command = bg.command(
+    Exercises.Commands.ExerciseAddCommand,
+    { payload: { id, absoluteFilePath: temporary.get(), name, description } },
+    deps,
+  );
+
+  await deps.CommandBus.emit(command);
 
   return new Response();
 };
