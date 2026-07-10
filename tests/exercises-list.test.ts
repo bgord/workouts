747bd7e7572f9ffe494c2ce5267322f864f1a92a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-07-10 12:21:36 +0200

    Add exercises list endpoint test

---
+++ tests/exercises-list.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, spyOn, test } from "bun:test";
+import * as bg from "@bgord/bun";
+import { bootstrap } from "+infra/bootstrap";
+import { createServer } from "../server";
+import * as mocks from "./mocks";
+
+const url = "/api/exercises/list";
+
+describe(`GET ${url}`, async () => {
+  const di = await bootstrap();
+  const server = createServer(di);
+
+  test("validation - AccessDeniedAuthShieldError", async () => {
+    const response = await server.request(url, { method: "GET" }, mocks.ip);
+    const json = await response.json();
+
+    expect(response.status).toEqual(403);
+    expect(json).toEqual({ message: bg.ShieldAuthStrategyError.Rejected, _known: true });
+  });
+
+  test("happy path", async () => {
+    const spies = new DisposableStack();
+    spies.use(spyOn(di.Tools.Auth.config.api, "getSession").mockResolvedValue(mocks.auth));
+    spies.use(spyOn(di.Adapters.Exercises.ListExercisesQuery, "execute").mockResolvedValue([mocks.exercise]));
+
+    const response = await server.request(url, { method: "GET", headers: mocks.revisionHeaders() }, mocks.ip);
+    const json = await response.json();
+
+    expect(response.status).toEqual(200);
+    expect(json).toEqual([mocks.exercise]);
+  });
+
+  test("happy path - empty", async () => {
+    const spies = new DisposableStack();
+    spies.use(spyOn(di.Tools.Auth.config.api, "getSession").mockResolvedValue(mocks.auth));
+    spies.use(spyOn(di.Adapters.Exercises.ListExercisesQuery, "execute").mockResolvedValue([]));
+
+    const response = await server.request(url, { method: "GET" }, mocks.ip);
+    const json = await response.json();
+
+    expect(response.status).toEqual(200);
+    expect(json).toEqual([]);
+  });
+});
