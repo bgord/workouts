84a7a738e8d2aa5dd9564464cfe1c3a702b33584
Bartosz Gordon <gordonbartosz@gmail.com>
2026-07-24 22:15:09 +0200

    Model reps VO

--- modules/plans/value-objects/reps.ts
+++ modules/plans/value-objects/reps.ts
@@ -1,5 +1,11 @@
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
 
-export const Reps = v.pipe(tools.IntegerPositive, v.brand("Reps"));
+export const RepsError = { Range: "reps.range" };
+
+export const Reps = v.pipe(
+  v.object({ min: tools.IntegerPositive, max: tools.IntegerPositive }),
+  v.check((value) => value.max >= value.min, RepsError.Range),
+  v.brand("Reps"),
+);
 export type RepsType = v.InferOutput<typeof Reps>;
