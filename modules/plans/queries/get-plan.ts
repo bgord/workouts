eea9d3b3d1706225c4fd2a2bc3e41ae4909c032d
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-02 11:36:28 +0200

    Add get plan query

---
+++ modules/plans/queries/get-plan.ts
@@ -0,0 +1,6 @@
+import type * as Auth from "+auth";
+import type * as VO from "+plans/value-objects";
+
+export interface GetPlan {
+  execute(planId: VO.PlanIdType, userId: Auth.VO.UserIdType): Promise<VO.Plan | null>;
+}
--- modules/plans/queries/index.ts
+++ modules/plans/queries/index.ts
@@ -1,3 +1,4 @@
+export * from "./get-plan";
 export * from "./get-plan-editable-for-owner-count";
 export * from "./get-plan-name-for-owner-count";
 export * from "./list-plans";
