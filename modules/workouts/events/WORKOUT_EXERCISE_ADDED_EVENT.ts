a3db88531860f53a24dd9ac638103f6c047e2bd4
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-03 12:38:58 +0200

    Add workout exercise added event

---
+++ modules/workouts/events/WORKOUT_EXERCISE_ADDED_EVENT.ts
@@ -0,0 +1,21 @@
+import * as bg from "@bgord/bun";
+import * as v from "valibot";
+import * as Auth from "+auth";
+import * as Exercises from "+exercises";
+import * as VO from "+workouts/value-objects";
+
+export const WORKOUT_EXERCISE_ADDED_EVENT = "WORKOUT_EXERCISE_ADDED_EVENT";
+
+export const WorkoutExerciseAddedEvent = v.object({
+  ...bg.EventEnvelopeSchema,
+  name: v.literal(WORKOUT_EXERCISE_ADDED_EVENT),
+  payload: v.object({
+    workoutId: VO.WorkoutId,
+    workoutExerciseId: VO.WorkoutExerciseId,
+    exerciseId: Exercises.VO.ExerciseId,
+    prescription: VO.ExercisePrescription,
+    requesterId: Auth.VO.UserId,
+  }),
+});
+
+export type WorkoutExerciseAddedEventType = v.InferOutput<typeof WorkoutExerciseAddedEvent>;
--- modules/workouts/events/index.ts
+++ modules/workouts/events/index.ts
@@ -1,1 +1,2 @@
 export * from "./WORKOUT_CREATED_EVENT";
+export * from "./WORKOUT_EXERCISE_ADDED_EVENT";
