665b9f9a2b980947f672c6e04a6ed64c8a593192
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-03 18:20:07 +0200

    Use exercise image mime registry

--- infra/adapters/system/hash-file.adapter.ts
+++ infra/adapters/system/hash-file.adapter.ts
@@ -1,4 +1,6 @@
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import * as Exercises from "+exercises";
 import * as Preferences from "+preferences";
 
 type Dependencies = { FileInspection: bg.FileInspectionPort };
@@ -6,7 +8,10 @@ type Dependencies = { FileInspection: bg.FileInspectionPort };
 export function createHashFile(deps: Dependencies) {
   return new bg.HashFileSha256Adapter({
     HashContent: new bg.HashContentSha256Strategy(),
-    MimeRegistry: Preferences.VO.ProfileAvatarMimeRegistry,
+    MimeRegistry: new tools.MimeRegistry([
+      ...Preferences.VO.ProfileAvatarMimeRegistry.entries,
+      ...Exercises.VO.ExerciseImageMimeRegistry.entries,
+    ]),
     FileReaderText: new bg.FileReaderTextAdapter(),
     ...deps,
   });
--- infra/adapters/system/image-info.adapter.ts
+++ infra/adapters/system/image-info.adapter.ts
@@ -1,8 +1,16 @@
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import * as Exercises from "+exercises";
 import * as Preferences from "+preferences";
 
 type Dependencies = { FileInspection: bg.FileInspectionPort };
 
 export function createImageInfo(deps: Dependencies): bg.ImageInfoPort {
-  return new bg.ImageInfoAdapter({ MimeRegistry: Preferences.VO.ProfileAvatarMimeRegistry, ...deps });
+  return new bg.ImageInfoAdapter({
+    MimeRegistry: new tools.MimeRegistry([
+      ...Preferences.VO.ProfileAvatarMimeRegistry.entries,
+      ...Exercises.VO.ExerciseImageMimeRegistry.entries,
+    ]),
+    ...deps,
+  });
 }
