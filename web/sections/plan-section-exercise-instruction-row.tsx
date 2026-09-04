ddfd7f55ec37d8aedc4d0e6508ff143d5610bec2
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-04 09:35:45 +0200

    Fix sets and reps alignment

--- web/sections/plan-section-exercise-instruction-row.tsx
+++ web/sections/plan-section-exercise-instruction-row.tsx
@@ -66,35 +66,37 @@ export function PlanSectionExerciseInstructionRow(props: {
         </div>
       )}
 
-      <div data-cross="center" data-gap="3" data-ml="auto" data-stack="x">
-        {editable && change.off && (
-          <PlanSectionExerciseInstructionUpdate
-            exerciseInstruction={exerciseInstruction}
-            plan={props.plan}
-            section={props.section}
-            toggle={update}
-          >
-            {instruction}
-          </PlanSectionExerciseInstructionUpdate>
-        )}
+      {editable && (
+        <div data-cross="center" data-gap="3" data-ml="auto" data-stack="x">
+          {change.off && (
+            <PlanSectionExerciseInstructionUpdate
+              exerciseInstruction={exerciseInstruction}
+              plan={props.plan}
+              section={props.section}
+              toggle={update}
+            >
+              {instruction}
+            </PlanSectionExerciseInstructionUpdate>
+          )}
 
-        {editable && update.off && (
-          <PlanSectionExerciseInstructionExerciseChange
-            exerciseInstruction={exerciseInstruction}
-            plan={props.plan}
-            section={props.section}
-            toggle={change}
-          />
-        )}
+          {update.off && (
+            <PlanSectionExerciseInstructionExerciseChange
+              exerciseInstruction={exerciseInstruction}
+              plan={props.plan}
+              section={props.section}
+              toggle={change}
+            />
+          )}
 
-        {editable && update.off && change.off && (
-          <PlanSectionExerciseInstructionRemove
-            exerciseInstruction={exerciseInstruction}
-            plan={props.plan}
-            section={props.section}
-          />
-        )}
-      </div>
+          {update.off && change.off && (
+            <PlanSectionExerciseInstructionRemove
+              exerciseInstruction={exerciseInstruction}
+              plan={props.plan}
+              section={props.section}
+            />
+          )}
+        </div>
+      )}
     </li>
   );
 }
