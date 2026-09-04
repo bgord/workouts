85b572fe2bebd9f42fd77baab3dcca0c9281597b
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-04 11:53:02 +0200

    Add workout get api

--- web/api/workouts.api.ts
+++ web/api/workouts.api.ts
@@ -1,4 +1,5 @@
 import { absoluteUrl, Cookies } from "@bgord/ui";
+import type { Workout } from "../../modules/workouts/value-objects/workout";
 import type { WorkoutSummary } from "../../modules/workouts/value-objects/workout-summary";
 
 export class Workouts {
@@ -11,6 +12,18 @@ export class Workouts {
     const response = await fetch(url, { headers, credentials: "include" });
 
     if (!response?.ok) return [];
+    return response.json().catch();
+  }
+
+  static async get(request: Request | null, params: { workoutId: string }): Promise<Workout | null> {
+    const BASE = `/api/workouts/${params.workoutId}`;
+
+    const url = absoluteUrl(BASE, request);
+    const headers = request ? { cookie: Cookies.extractFrom(request) } : undefined;
+
+    const response = await fetch(url, { headers, credentials: "include" });
+
+    if (!response?.ok) return null;
     return response.json().catch();
   }
 }
