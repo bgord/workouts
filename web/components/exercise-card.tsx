ed88d5e6f22f33ed766627de9b8d8df94ea9fb6f
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-07 11:20:55 +0200

    Improve exercise card

--- web/components/exercise-card.tsx
+++ web/components/exercise-card.tsx
@@ -5,53 +5,31 @@ import { ExerciseImage, ExerciseImageSize } from "./exercise-image";
 
 export function ExerciseCard(props: { exercise: ExerciseWithCategories }) {
   return (
-    <li
-      data-bc="neutral-800"
-      data-bg="neutral-800"
-      data-br="md"
-      data-bs="solid"
-      data-bw="hairline"
-      data-gap="2"
-      data-hover-bc="brand-800"
-      data-hover-shadow="md"
-      data-maxw="100%"
-      data-p="3"
-      data-shadow="sm"
-      data-stack="y"
-      {...bg.Rhythm(232).times(1).style.width}
-    >
+    <li data-maxw="100%" data-stack="y" {...bg.Rhythm(232).times(1).style.width}>
       <Link
-        aria-hidden
-        data-disp="flex"
-        params={{ exerciseId: props.exercise.id }}
-        tabIndex={-1}
-        to="/catalog/exercise/$exerciseId"
-      >
-        <ExerciseImage exercise={props.exercise} size={ExerciseImageSize.md} />
-      </Link>
-
-      <Link
-        data-color="neutral-0"
-        data-focus-ring="neutral"
-        data-fs="base"
-        data-fw="bold"
-        data-hover-color="brand-300"
-        data-maxw="100%"
-        data-transform="truncate"
+        className="c-card"
+        data-gap="3"
+        data-grow="1"
         params={{ exerciseId: props.exercise.id }}
         title={props.exercise.name}
         to="/catalog/exercise/$exerciseId"
       >
-        {props.exercise.name}
-      </Link>
+        <span aria-hidden data-disp="flex">
+          <ExerciseImage exercise={props.exercise} size={ExerciseImageSize.md} />
+        </span>
 
-      <ul data-gap="1" data-mt="auto" data-overflow="hidden" data-stack="x" data-wrap="nowrap">
-        {props.exercise.categories.map((category) => (
-          <li className="c-badge" data-variant="outline" key={category.id}>
-            {category.name}
-          </li>
-        ))}
-      </ul>
+        <div className="c-card-title" data-maxw="100%" data-transform="truncate">
+          {props.exercise.name}
+        </div>
+
+        <ul data-gap="1" data-mt="auto" data-overflow="hidden" data-stack="x" data-wrap="nowrap">
+          {props.exercise.categories.map((category) => (
+            <li className="c-badge" data-variant="outline" key={category.id}>
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </Link>
     </li>
   );
 }
