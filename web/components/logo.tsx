da1f3062b38d37aa498454ccbab5aa8824add514
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-08 13:20:54 +0200

    Improve logo

--- web/components/logo.tsx
+++ web/components/logo.tsx
@@ -1,26 +1,21 @@
-import { useTranslations } from "@bgord/ui";
 import { Link } from "@tanstack/react-router";
 import { Form } from "../../app/services/workout-history-filters-form";
 
 export function Logo() {
-  const t = useTranslations();
-
   return (
-    <div
-      className="logo"
-      data-color="brand-500"
-      data-cross="center"
-      data-disp="flex"
-      data-fs="2xl"
-      data-fw="bold"
-      data-gap="2"
-      data-lh="none"
-      data-ls="wider"
-      data-transform="uppercase"
-    >
-      <Link search={Form.default} to="/">
-        {t("app.name")}
-      </Link>
-    </div>
+    <Link search={Form.default} to="/">
+      <div
+        className="logo"
+        data-color="brand-500"
+        data-cross="center"
+        data-disp="flex"
+        data-fs="2xl"
+        data-fw="bold"
+        data-gap="2"
+        data-lh="none"
+        data-ls="wider"
+        data-transform="uppercase"
+      />
+    </Link>
   );
 }
