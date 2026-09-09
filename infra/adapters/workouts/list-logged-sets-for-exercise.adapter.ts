24ff44522588b11d19b74d81dce550c9c070e4ac
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-09 12:52:38 +0200

    Add list logged sets for exercise query adapter

---
+++ infra/adapters/workouts/list-logged-sets-for-exercise.adapter.ts
@@ -0,0 +1,31 @@
+import { and, eq } from "drizzle-orm";
+import type * as Auth from "+auth";
+import type * as Exercises from "+exercises";
+import type * as Workouts from "+workouts";
+import { db } from "+infra/db";
+import * as Schema from "+infra/schema";
+
+class ListLoggedSetsForExerciseQueryDrizzle implements Workouts.Queries.ListLoggedSetsForExercise {
+  async execute(
+    userId: Auth.VO.UserIdType,
+    exerciseId: Exercises.VO.ExerciseIdType,
+  ): Promise<Array<Workouts.Queries.LoggedSetForExercise>> {
+    return db
+      .select({
+        id: Schema.workoutLoggedSets.id,
+        workoutId: Schema.workoutLoggedSets.workoutId,
+        reps: Schema.workoutLoggedSets.reps,
+        load: Schema.workoutLoggedSets.load,
+      })
+      .from(Schema.workoutLoggedSets)
+      .innerJoin(
+        Schema.workoutExercises,
+        eq(Schema.workoutLoggedSets.workoutExerciseId, Schema.workoutExercises.id),
+      )
+      .where(
+        and(eq(Schema.workoutLoggedSets.userId, userId), eq(Schema.workoutExercises.exerciseId, exerciseId)),
+      );
+  }
+}
+
+export const ListLoggedSetsForExerciseQuery = new ListLoggedSetsForExerciseQueryDrizzle();
