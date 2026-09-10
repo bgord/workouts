df94de3fb1f849c8d8c48647e217962a4d073653
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-10 14:15:14 +0200

    Improve kdb

--- web/components/kbd.tsx
+++ web/components/kbd.tsx
@@ -1,24 +1,16 @@
 export function Kbd(props: { keys: string }) {
   return (
-    <kbd data-cross="center" data-gap="1" data-stack="x">
-      {props.keys.split(" ").map((key) => (
-        <span
-          data-bc="alpha-soft"
-          data-bg="alpha-subtle"
-          data-br="sm"
-          data-bs="solid"
-          data-bw="hairline"
-          data-color="neutral-300"
-          data-ff="mono"
-          data-fs="xs"
-          data-lh="snug"
-          data-px="1"
-          data-py="0-5"
-          key={key}
-        >
-          {key}
-        </span>
-      ))}
-    </kbd>
+    <span
+      data-bg="brand-900"
+      data-br="sm"
+      data-color="brand-300"
+      data-ff="mono"
+      data-fs="xs"
+      data-fw="medium"
+      data-ls="wide"
+      data-px="1"
+    >
+      {props.keys}
+    </span>
   );
 }
