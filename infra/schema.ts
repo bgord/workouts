d43a64f68115dff9814af9dab058b62f30ab2d5d
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-10 21:49:28 +0200

    Add image etag to exercises schema

--- infra/schema.ts
+++ infra/schema.ts
@@ -183,6 +183,7 @@ export const exercises = sqliteTable("exercises", {
   name: text("name").notNull().$type<ExerciseNameType>(),
   description: text("description").notNull().$type<ExerciseDescriptionType>(),
   image: text("image").notNull().$type<tools.ObjectKeyType>(),
+  imageEtag: text("imageEtag").notNull().$type<bg.HashValueType>(),
   userId: text("userId", { length: 36 }).notNull().$type<UserIdType>(),
   createdAt: timestamp("createdAt").notNull(),
   updatedAt: timestamp("updatedAt").notNull(),
