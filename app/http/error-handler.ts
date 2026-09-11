fb71843aaf7e67b85b58e0daa005bb3e4a7178f4
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-11 21:37:55 +0200

    Add missing revision error mapping

--- app/http/error-handler.ts
+++ app/http/error-handler.ts
@@ -14,6 +14,7 @@ const messages = new bg.ErrorClassifierMessageMapStrategy({
     status: 400,
   },
   [tools.RevisionError.Mismatch]: { message: "revision.mismatch", status: 412 },
+  [tools.RevisionError.Missing]: { message: "revision.missing", status: 428 },
 });
 
 const http = new bg.ErrorClassifierHttpExceptionHonoStrategy([bg.HttpExceptionErrors]);
