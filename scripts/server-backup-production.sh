cf5c250de840eb2b1b31963e03edcc6325f0ffa9
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-12 20:31:49 +0200

    Clean up server backup production

--- scripts/server-backup-production.sh
+++ scripts/server-backup-production.sh
@@ -11,9 +11,6 @@ BACKUPS_PATH="/var/backups/$PROJECT_NAME"
 DATABASE_PATH="/var/www/$PROJECT_NAME/sqlite.db"
 DATABASE_BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.sqlite.backup"
 
-JOBS_PATH="/var/www/$PROJECT_NAME/jobs.db"
-JOBS_BACKUP_PATH="$BACKUPS_PATH/$CURRENT_TIME.jobs.backup"
-
 STORAGE_DIR="/var/www/$PROJECT_NAME/infra/storage"
 STORAGE_ARCHIVE_PATH="$BACKUPS_PATH/$CURRENT_TIME.storage.tar.gz"
 
@@ -35,14 +32,6 @@ echo "Database backup created to: $DATABASE_BACKUP_PATH"
 tar -czf "$DATABASE_BACKUP_PATH.tar.gz" "$DATABASE_BACKUP_PATH"
 echo "Database backup compressed"
 rm "$DATABASE_BACKUP_PATH"
-
-echo "Creating a jobs backup: $JOBS_PATH"
-sqlite3 "$JOBS_PATH" ".backup $JOBS_BACKUP_PATH"
-echo "Jobs backup created to: $JOBS_BACKUP_PATH"
-
-tar -czf "$JOBS_BACKUP_PATH.tar.gz" "$JOBS_BACKUP_PATH"
-echo "Jobs backup compressed"
-rm "$JOBS_BACKUP_PATH"
 
 echo "Archiving storage from: $STORAGE_DIR"
 tar -czf "$STORAGE_ARCHIVE_PATH" -C "$STORAGE_DIR" .
