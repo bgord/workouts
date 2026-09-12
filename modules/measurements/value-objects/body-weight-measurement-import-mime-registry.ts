429b74de9bbb41decf279802308e37a48245096c
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-12 14:57:10 +0200

    Add body weight measurement import mime

---
+++ modules/measurements/value-objects/body-weight-measurement-import-mime-registry.ts
@@ -0,0 +1,3 @@
+import * as tools from "@bgord/tools";
+
+export const BodyWeightMeasurementImportMimeRegistry = new tools.MimeRegistry([tools.Mimes.csv]);
--- modules/measurements/value-objects/index.ts
+++ modules/measurements/value-objects/index.ts
@@ -2,3 +2,5 @@ export * from "./body-weight";
 export * from "./body-weight-measured-on";
 export * from "./body-weight-measurement";
 export * from "./body-weight-measurement-id";
+export * from "./body-weight-measurement-import-max-size";
+export * from "./body-weight-measurement-import-mime-registry";
