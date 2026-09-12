b6a1fd2541d24de52537f0c19d25dfdbb37944ee
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-12 23:16:37 +0200

    Fix body weight measurement list history

--- web/sections/body-weight-measurement-list.tsx
+++ web/sections/body-weight-measurement-list.tsx
@@ -3,7 +3,15 @@ import { BodyWeightMeasurementRow } from "./body-weight-measurement-row";
 
 export function BodyWeightMeasurementList(props: { measurements: ReadonlyArray<BodyWeightMeasurement> }) {
   return (
-    <ul className="c-card" data-md-p="2-5" data-p="4" data-stack="y" data-variant="flat">
+    <ul
+      className="c-card"
+      data-md-bw="none"
+      data-md-p="2-5"
+      data-md-px="0"
+      data-p="4"
+      data-stack="y"
+      data-variant="flat"
+    >
       {props.measurements.map((measurement, index) => (
         <BodyWeightMeasurementRow
           index={index}
