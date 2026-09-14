a921e6dcd3f1d004ca65ed82a921413ae5a49b19
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-14 22:14:49 +0200

    Add goal to body weight measurements command

--- modules/measurements/commands/BODY_WEIGHT_REFERENCE_SET_COMMAND.ts
+++ modules/measurements/commands/BODY_WEIGHT_REFERENCE_SET_COMMAND.ts
@@ -1,6 +1,7 @@
 import * as bg from "@bgord/bun";
 import * as v from "valibot";
 import * as Auth from "+auth";
+import { BodyWeightGoal } from "../value-objects/body-weight-goal";
 import { BodyWeightMeasurementId } from "../value-objects/body-weight-measurement-id";
 
 // Stryker disable next-line StringLiteral
@@ -9,7 +10,11 @@ export const BODY_WEIGHT_REFERENCE_SET_COMMAND = "BODY_WEIGHT_REFERENCE_SET_COMM
 export const BodyWeightReferenceSetCommand = v.object({
   ...bg.CommandEnvelopeSchema,
   name: v.literal(BODY_WEIGHT_REFERENCE_SET_COMMAND),
-  payload: v.object({ measurementId: BodyWeightMeasurementId, requesterId: Auth.VO.UserId }),
+  payload: v.object({
+    measurementId: BodyWeightMeasurementId,
+    goal: BodyWeightGoal,
+    requesterId: Auth.VO.UserId,
+  }),
 });
 
 export type BodyWeightReferenceSetCommandType = v.InferOutput<typeof BodyWeightReferenceSetCommand>;
