6dcec65563a62fadd8238bc8b1bf90226bd8436e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-14 13:52:00 +0200

    Wire up filter in workout list

--- app/http/workouts/workout-list.ts
+++ app/http/workouts/workout-list.ts
@@ -1,14 +1,20 @@
 import type * as bg from "@bgord/bun";
-import type * as Workouts from "+workouts";
+import * as v from "valibot";
+import * as Workouts from "+workouts";
 
 type Dependencies = { ListWorkoutsQuery: Workouts.Queries.ListWorkouts };
 
 export const WorkoutList =
   (deps: Dependencies): bg.EndpointPort =>
   async (context) => {
-    const userId = context.identity.authenticatedUserId();
+    const body = await context.request.json();
 
-    const workouts = await deps.ListWorkoutsQuery.execute(userId);
+    const userId = context.identity.authenticatedUserId();
+    const filter =
+      v.parse(Workouts.VO.WorkoutListFilter, body["filter"]) ??
+      Workouts.VO.WorkoutListFilterOptions.last_week;
+
+    const workouts = await deps.ListWorkoutsQuery.execute(userId, filter);
 
     return Response.json(workouts);
   };
--- modules/workouts/value-objects/workout-list-filter.ts
+++ modules/workouts/value-objects/workout-list-filter.ts
@@ -3,5 +3,5 @@ import { WorkoutListFilterOptions } from "./workout-list-filter-options";
 
 export const WorkoutListFilterError = { invalid: "workout.list.filter.invalid" };
 
-export const WorkoutListFilter = v.enum(WorkoutListFilterOptions, WorkoutListFilterError.invalid);
+export const WorkoutListFilter = v.optional(v.enum(WorkoutListFilterOptions, WorkoutListFilterError.invalid));
 export type WorkoutListFilterType = v.InferOutput<typeof WorkoutListFilter>;
