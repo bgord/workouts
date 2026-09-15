21927c0d39a6945296dc9c60637fac51be720ccc
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-15 10:27:34 +0200

    Fix bottom mobile spacing

--- web/components/main.tsx
+++ web/components/main.tsx
@@ -4,6 +4,7 @@ export function Main(props: React.JSX.IntrinsicElements["main"]) {
       data-gap="6"
       data-maxw="md"
       data-md-m="2"
+      data-md-mb="8"
       data-md-mt="0"
       data-md-pt="4"
       data-md-px="0"
