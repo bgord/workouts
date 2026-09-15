9b414fc5e00aff815ae1983ad12862ddfb22deac
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-15 11:31:25 +0200

    Fix close buttons

--- web/components/button-close.tsx
+++ web/components/button-close.tsx
@@ -1,8 +1,19 @@
+import { useTranslations } from "@bgord/ui";
 import { X } from "lucide-react";
 
 export function ButtonClose(props: React.JSX.IntrinsicElements["button"]) {
+  const t = useTranslations();
+
   return (
-    <button className="c-button" data-interaction="subtle-scale" data-variant="icon" type="button" {...props}>
+    <button
+      aria-label={t("app.close")}
+      className="c-button"
+      data-interaction="subtle-scale"
+      data-variant="icon"
+      title={t("app.close")}
+      type="button"
+      {...props}
+    >
       <X data-size="md" />
     </button>
   );
--- web/sections/profile-avatar-delete.tsx
+++ web/sections/profile-avatar-delete.tsx
@@ -1,9 +1,11 @@
-import { useMutation, useToggle } from "@bgord/ui";
+import { useMutation, useToggle, useTranslations } from "@bgord/ui";
 import { useRouter } from "@tanstack/react-router";
-import { Avatar, AvatarSize, ButtonClose } from "../components";
+import { X } from "lucide-react";
+import { Avatar, AvatarSize } from "../components";
 import { rootRoute } from "../router";
 
 export function ProfileAvatarDelete() {
+  const t = useTranslations();
   const router = useRouter();
   const { avatarEtag } = rootRoute.useLoaderData();
   const overlay = useToggle({ name: "profile-avatar-delete" });
@@ -16,21 +18,32 @@ export function ProfileAvatarDelete() {
   });
 
   return (
-    <button data-cursor="pointer" data-position="relative" onClick={overlay.toggle} type="button">
-      <Avatar size={AvatarSize.lg} />
+    <div data-position="relative" data-self="start">
+      <button data-cursor="pointer" data-disp="block" onClick={overlay.toggle} type="button">
+        <Avatar size={AvatarSize.lg} />
+      </button>
 
       {overlay.on && enabled && (
         <div data-bg="neutral-900" data-br="md" data-inset="0" data-opacity="high" data-position="absolute" />
       )}
       {overlay.on && enabled && (
-        <ButtonClose
+        <button
+          className="c-button"
+          data-color="neutral-200"
+          data-hover-color="danger-400"
           data-left="5"
           data-position="absolute"
           data-right="5"
           data-top="8"
+          data-variant="ghost"
+          disabled={mutation.isLoading}
           onClick={() => mutation.mutate()}
-        />
+          title={t("profile.avatar.delete.title")}
+          type="button"
+        >
+          <X data-size="md" />
+        </button>
       )}
-    </button>
+    </div>
   );
 }
