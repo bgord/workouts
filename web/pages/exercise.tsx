bab18a7206b0c796fd7e674537b80430d48e72b3
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-16 10:23:16 +0200

    Improve exercise hero

--- web/pages/exercise.tsx
+++ web/pages/exercise.tsx
@@ -96,15 +96,7 @@ export function Exercise() {
         )}
       </div>
 
-      <div
-        className="c-card"
-        data-cross="start"
-        data-gap="5"
-        data-md-p="2-5"
-        data-stack="x"
-        data-variant="flat"
-        data-wrap="wrap"
-      >
+      <div data-cross="start" data-gap="5" data-px="4" data-stack="x" data-wrap="wrap">
         <div data-md-grow="1" style={image}>
           {exercise.actions.imageChange.enabled ? (
             <ExerciseImageChange exercise={exercise.data} />
@@ -160,6 +152,7 @@ export function Exercise() {
           className="c-card"
           data-cross="center"
           data-gap="1"
+          data-mx="4"
           data-py="8"
           data-stack="y"
           data-variant="flat"
@@ -177,7 +170,7 @@ export function Exercise() {
       )}
 
       {performances.length > 0 && (
-        <div data-gap="6" data-stack="y">
+        <div data-gap="6" data-px="4" data-stack="y">
           <ExerciseStats performances={performances} />
 
           <ExerciseProgressChart performances={performances} />
