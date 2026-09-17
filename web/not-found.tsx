3345e41deb6d22664c25d467f83df3a80ac5aaf5
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-17 10:58:07 +0200

    Improve not found

--- web/not-found.tsx
+++ web/not-found.tsx
@@ -1,16 +1,16 @@
 import { useTranslations } from "@bgord/ui";
 import { Link } from "@tanstack/react-router";
 import { Form as WorkoutHistoryFilters } from "../app/services/workout-history-filters-form";
-import { Main } from "./components";
+import { Header, Main } from "./components";
 
 export function NotFound() {
   const t = useTranslations();
 
   return (
     <Main>
-      <h1 data-color="neutral-0" data-fs="2xl" data-fw="black" data-md-fs="xl">
+      <Header data-color="neutral-0" data-fs="2xl" data-fw="black" data-md-fs="xl">
         {t("app.not_found.header")}
-      </h1>
+      </Header>
 
       <div data-color="neutral-400">{t("app.not_found.info")}</div>
 
