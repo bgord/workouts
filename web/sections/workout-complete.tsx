f4fcce1bd7430ae3b108753d73014fbf27f94835
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-18 13:20:33 +0200

    Simplify workout complete

--- web/sections/workout-complete.tsx
+++ web/sections/workout-complete.tsx
@@ -22,14 +22,7 @@ export function WorkoutComplete() {
   if (!workout.actions.complete.available) return null;
 
   return (
-    <form
-      aria-busy={mutation.isLoading}
-      data-cross="center"
-      data-stack="x"
-      data-wrap="nowrap"
-      onSubmit={mutation.handleSubmit}
-      {...ui.Gap.related}
-    >
+    <form aria-busy={mutation.isLoading} data-stack="y" onSubmit={mutation.handleSubmit} {...ui.Gap.cluster}>
       <button
         className="c-button"
         data-variant="primary"
