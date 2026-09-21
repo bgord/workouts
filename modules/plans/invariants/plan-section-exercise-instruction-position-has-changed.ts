f6d1ab65100ce3467514fa2e16844109c4d29d24
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-21 21:55:06 +0200

    Add PlanSectionExerciseInstructionPositionHasChanged invariant

---
+++ modules/plans/invariants/plan-section-exercise-instruction-position-has-changed.ts
@@ -0,0 +1,28 @@
+import * as bg from "@bgord/bun";
+import type * as VO from "+plans/value-objects";
+
+class PlanSectionExerciseInstructionPositionHasChangedError extends Error {}
+
+type PlanSectionExerciseInstructionPositionHasChangedConfigType = {
+  planSection: VO.PlanSection | undefined;
+  exerciseInstructionId: VO.ExerciseInstructionIdType;
+  position: VO.ExerciseInstructionPositionType;
+};
+
+class PlanSectionExerciseInstructionPositionHasChangedFactory extends bg.Invariant<PlanSectionExerciseInstructionPositionHasChangedConfigType> {
+  passes(config: PlanSectionExerciseInstructionPositionHasChangedConfigType) {
+    const current = config.planSection?.exerciseInstructions.findIndex(
+      (exerciseInstruction) => exerciseInstruction.id === config.exerciseInstructionId,
+    );
+
+    return current !== config.position;
+  }
+
+  // Stryker disable next-line StringLiteral
+  message = "plan.section.exercise.instruction.position.has.changed";
+  error = PlanSectionExerciseInstructionPositionHasChangedError;
+  kind = bg.InvariantFailureKind.forbidden;
+}
+
+export const PlanSectionExerciseInstructionPositionHasChanged =
+  new PlanSectionExerciseInstructionPositionHasChangedFactory();
