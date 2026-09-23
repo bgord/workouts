c5ab0823f3cb8e95adcdcb1fc90d056a25f04ab7
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-23 23:57:20 +0200

    Simplify row index

--- web/components/row-index.tsx
+++ web/components/row-index.tsx
@@ -1,3 +1,3 @@
-export function RowIndex(props: React.JSX.IntrinsicElements["div"]) {
-  return <div data-color="neutral-600" data-fs="xs" data-transform="font-variant-numeric" {...props} />;
+export function RowIndex(props: React.JSX.IntrinsicElements["small"]) {
+  return <small data-color="neutral-600" {...props} />;
 }
