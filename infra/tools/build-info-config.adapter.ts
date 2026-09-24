b00e410d0e332187e4ff79a57949626f82ae826a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-25 00:17:27 +0200

    Temporarily adjust build info

--- infra/tools/build-info-config.adapter.ts
+++ infra/tools/build-info-config.adapter.ts
@@ -2,27 +2,33 @@ import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
 import type { EnvironmentResultType } from "+infra/env";
+import { WebAssets } from "./web-assets.vo";
 
 type Dependencies = { Clock: bg.ClockPort; FileReaderJson: bg.FileReaderJsonPort };
+
+const BuildInfo = v.object({ ...bg.BuildInfo.entries, assets: WebAssets });
+
+type BuildInfoType = v.InferOutput<typeof BuildInfo>;
 
 export function createBuildInfoConfig(
   Env: EnvironmentResultType,
   deps: Dependencies,
-): bg.ReactiveConfigPort<bg.BuildInfoType> {
-  const noop = new bg.ReactiveConfigNoopAdapter<bg.BuildInfoType>(bg.BuildInfo, {
+): bg.ReactiveConfigPort<BuildInfoType> {
+  const noop = new bg.ReactiveConfigNoopAdapter<BuildInfoType>(BuildInfo, {
     timestamp: tools.Timestamp.fromNumber(1767775662000).ms,
     version: v.parse(tools.PackageVersionSchema, "v1.0.0"),
     sha: bg.CommitSha.fromString("a".repeat(40)).value,
     size: tools.Size.fromBytes(0).toBytes(),
+    assets: { entry: "/public/entry-client.js", preloads: {} },
   });
 
   return {
     [bg.NodeEnvironmentEnum.local]: noop,
     [bg.NodeEnvironmentEnum.test]: noop,
     [bg.NodeEnvironmentEnum.staging]: noop,
-    [bg.NodeEnvironmentEnum.production]: new bg.ReactiveConfigFileJsonAdapter<bg.BuildInfoType>(
+    [bg.NodeEnvironmentEnum.production]: new bg.ReactiveConfigFileJsonAdapter<BuildInfoType>(
       bg.BUILD_INFO_FILE_PATH,
-      bg.BuildInfo,
+      BuildInfo,
       deps,
     ),
   }[Env.type];
