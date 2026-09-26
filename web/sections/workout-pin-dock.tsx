76c61e4233cc94873eacdf0a75c61a519c8dc54a
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-26 09:50:34 +0200

    Render workout exercise row in the dock

--- web/sections/workout-pin-dock.tsx
+++ web/sections/workout-pin-dock.tsx
@@ -1,51 +1,65 @@
+import * as bg from "@bgord/ui";
+import type { WorkoutExercise } from "../../modules/workouts/queries/get-workout";
 import * as ui from "../components";
+import { useOptimisticSet } from "../hooks/use-optimistic-set";
 import type { UsePinnedExerciseReturnType } from "../hooks/use-pinned-exercise";
 import { workoutRoute } from "../router";
+import { WorkoutSetList } from "./workout-set-list";
+import { WorkoutSetLog } from "./workout-set-log";
 
 export function WorkoutPinDock(props: UsePinnedExerciseReturnType) {
   const { workout } = workoutRoute.useLoaderData();
 
-  const exercise = workout.data.exercises.find(
-    (exercise) => exercise.id === props.id && exercise.actions.setLog.available,
-  );
+  const exercise = workout.data.exercises.find(props.isPinned);
 
   if (!exercise) return null;
 
+  return <WorkoutPinDockPanel exercise={exercise} key={exercise.id} onClose={props.unpin} />;
+}
+
+function WorkoutPinDockPanel(props: { exercise: WorkoutExercise; onClose: VoidFunction }) {
+  const { exercise, pendingSet, setPendingSet } = useOptimisticSet(props.exercise);
+
   return (
-    <aside
-      data-bottom="0"
-      data-left="0"
-      data-maxw="md"
-      data-md-px="0"
-      data-mx="auto"
-      data-pin-dock
-      data-position="fixed"
-      data-px="3"
-      data-right="0"
-      data-z="2"
-    >
+    <aside data-bottom="4" data-pin-dock data-position="sticky" data-z="2">
       <div
         data-bc="alpha-medium"
         data-bg="neutral-900"
         data-br="lg"
         data-bs="solid"
         data-bw="hairline"
-        data-cross="center"
-        data-mb="4"
         data-md-br="none"
         data-md-bwb="none"
         data-md-bwx="none"
-        data-md-mb="0"
         data-p="3"
         data-shadow="lg"
-        data-stack="x"
+        data-stack="y"
         {...ui.Gap.related}
       >
-        <strong data-color="neutral-100" data-grow="1" data-minw="0" data-transform="truncate">
-          {exercise.exerciseName}
-        </strong>
+        <div data-cross="center" data-stack="x" {...ui.Gap.related}>
+          <ui.ExerciseImage
+            id={exercise.exerciseId}
+            imageEtag={exercise.exerciseImageEtag}
+            name={exercise.exerciseName}
+            size={ui.ExerciseImageSize.xs}
+          />
 
-        <ui.ButtonClose onClick={props.unpin} />
+          <strong data-color="neutral-100" data-grow="1" data-minw="0" data-transform="truncate">
+            {exercise.exerciseName}
+          </strong>
+
+          {exercise.target && <ui.SetDots sets={exercise.loggedSets} target={exercise.target.sets} />}
+
+          <ui.ButtonClose onClick={props.onClose} />
+        </div>
+
+        {exercise.loggedSets.length > 0 && (
+          <div data-overflow="auto" {...bg.Rhythm().times(18).style.maxHeight}>
+            <WorkoutSetList exercise={exercise} pendingSet={pendingSet} />
+          </div>
+        )}
+
+        <WorkoutSetLog exercise={exercise} onPending={setPendingSet} />
       </div>
     </aside>
   );
